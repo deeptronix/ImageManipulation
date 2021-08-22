7920ef72181808a64fd0e2ceb605e1167b8dd284
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-08-22 18:25:45 +0200

    Update ImageManipulation.cpp

--- ImageManipulation.cpp
+++ ImageManipulation.cpp
@@ -6,11 +6,26 @@ WARNING: These algorithms operate on the microcontroller's RAM buffer
 				 as the image buffer itself) is required to avoid crashes.
 
 
-Written in 2020 by Leopoldo Perizzolo: find me @ Deep Tronix:
-https://rebrand.ly/deeptronix
-As long as you retain this notice you can do whatever you want with this stuff. 
-If we meet some day, and you think this stuff is worth it, 
-you can buy me a beer in return. ( https://en.wikipedia.org/wiki/Beerware ).
+Copyright (c) 2021 Leopoldo Perizzolo - Deep Tronix
+Find me @ https://rebrand.ly/deeptronix
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 ********************************************************************************/
 
 #include <stdlib.h>
